f4867a9f074e055e4c367f0ee81e008bff661344
EC2 Default User <ec2-user@ip-172-31-85-137.ec2.internal>
2022-01-26 21:09:04 +0000

    my lab2.py file

---
+++ Lec2.py
@@ -0,0 +1,17 @@
+
+#This is a regional comment
+
+#print('Hello, World') # This is a single line comment
+
+#print(type(123))
+
+#print(type(123.))
+
+#print(type("123"))
+
+#print("Hello, World")
+#print('Hello, World')
+
+print('Hello, World')
+print('Hello, World'.upper())
+print('Hello, World'.lower())
---
+++ lab2.py
@@ -0,0 +1,17 @@
+my_name = 'Tom'
+print(my_name.upper())
+my_id = (123)
+print(my_id)
+#123 = my_id
+my_id = your_id = 123
+print(your_id)
+my_id_str = ('123')
+print(my_id_str)
+#my_name + my_id 
+print(my_name + my_id_str)
+print(my_name * 3)
+print('hello, world. This is my first python string'.split('.'))
+
+message = ("Tom's id is 123")
+print(message)
+
